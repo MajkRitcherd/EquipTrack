17f129840ff3ce3e16aa8b3a0fb603098927ad45
MajkRitcherd <misutak87@gmail.com>
2026-09-17 13:29:46 +0200

    refactor: Add comments to user's properties

--- server/EquipTrack.Domain/User.cs
+++ server/EquipTrack.Domain/User.cs
@@ -9,9 +9,24 @@ namespace EquipTrack.Domain
     /// <param name="Email">User's email.</param>
     public class User
     {
+        /// <summary>
+        /// Gets user's email.
+        /// </summary>
         public required string Email { get; init; }
+
+        /// <summary>
+        /// Gets user's first name.
+        /// </summary>
         public required string FirstName { get; init; }
+
+        /// <summary>
+        /// Gets user's ID.
+        /// </summary>
         public required int Id { get; init; }
+
+        /// <summary>
+        /// Gets user's last name.
+        /// </summary>
         public required string LastName { get; init; }
     }
 }
