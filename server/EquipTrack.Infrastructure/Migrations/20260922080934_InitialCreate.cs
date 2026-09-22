75497e9a78385610d44d3f1e77014b657d2573a1
MajkRitcherd <misutak87@gmail.com>
2026-09-22 10:29:41 +0200

    feat: Generate initial migration with SQLite

---
+++ server/EquipTrack.Infrastructure/Migrations/20260922080934_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquipTrack.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    HardwareInfo_Cpu = table.Column<string>(type: "TEXT", nullable: true),
+                    HardwareInfo_IntegratedGpu = table.Column<string>(type: "TEXT", nullable: true),
+                    HardwareInfo_DedicatedGpu = table.Column<string>(type: "TEXT", nullable: true),
+                    HardwareInfo_RamStorageInGB = table.Column<int>(type: "INTEGER", nullable: true),
+                    HardwareInfo_StorageInGB = table.Column<int>(type: "INTEGER", nullable: true),
+                    ManufacturerName = table.Column<string>(type: "TEXT", nullable: false),
+                    ModelName = table.Column<string>(type: "TEXT", nullable: false),
+                    SerialNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    State = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
