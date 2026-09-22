5b4f3a3b177ebbe717a39fb3c254b6556acc153b
MajkRitcherd <misutak87@gmail.com>
2026-09-22 09:46:39 +0200

    feat: Add NuGets for EF to Api project, Add DB context in the Api project

--- server/EquipTrack.Api/Program.cs
+++ server/EquipTrack.Api/Program.cs
@@ -1,8 +1,16 @@
+using EquipTrack.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite("Data Source=equiptrack.db");
+});
 
 var app = builder.Build();
 
